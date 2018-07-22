89c34845344f1ed426492522fec6801f90518145
shahedbd <shahed.mbstu@gmail.com>
2018-07-22 13:16:19 +0600

    + Added enums

---
+++ Sln.SearchMediaFiles/SearchMediaFiles/LIB/CommonEnums.cs
@@ -0,0 +1,17 @@
+﻿namespace SearchMediaFiles.LIB
+{
+    class CommonEnums
+    {
+    }
+
+    public enum MediaExtensionsTypes : int
+    {
+        Video = 0,
+        Audio = 1,
+        Image = 2,
+        All = 3,
+        Folders = 4
+    }
+
+
+}
